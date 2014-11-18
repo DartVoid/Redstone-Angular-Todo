b1bc87b0c49a3630a8ed8e814e7e38291bc1380e
Robert Åkerblom-Andersson <Robert@dartvoid.com>
2014-11-18 14:55:22 +0100

    Ported to DartVoid new file structure and Angular 1.0

--- lib/src/item.dart
+++ lib/src/item.dart
@@ -1,20 +1,18 @@
 part of client;
 
 class Item extends Schema {
-  
   @Id()
   String id;
-  
+
   @Field()
   @NotEmpty()
   String text;
-  
+
   @Field()
   bool done;
-  
+
   Item([this.id, this.text = "", this.done = false]);
-  
+
   String toString() => "Item($id, $text, $done)";
-  
 }
 
---
+++ web/items_backend.dart
@@ -0,0 +1,56 @@
+part of main;
+
+// Backend class that communicates with the backend and hold the items list
+@Injectable()
+class ItemsBackend {
+  BrowserClient _http = new BrowserClient();
+  String _baseUrl;
+
+  ItemsBackend() {
+    // Initialize baseUrl (9090 for during development locally, otherwise use
+    // standard port 80 for production, pub serve uses 8080)
+    Uri uri = Uri.parse(window.location.href);
+    var port = uri.port != 8080 ? 80 : 9090;
+    _baseUrl = 'http://${uri.host}:${port}';
+  }
+
+  Future<List<Item>> getAll() {
+    // Make request to get a list of all todo items
+    return _http.get('${_baseUrl}/todos/list').then((res) {
+      return decode(res.body, Item);
+    }).catchError((error) {
+      print("Got error $error");
+    });
+  }
+
+  Future add(Item item) {
+    print("In backend.add");
+
+    // Make request to add new item to database
+    return _http.post("${_baseUrl}/todos/add", body: JSON.encode(encode(item)))
+      .catchError((error) {
+        print("Got error $error");
+      });
+  }
+
+  Future update(Item item) {
+    print("In backend.update");
+
+    // Make request to update item
+    return _http.put("${_baseUrl}/todos/update", body: JSON.encode(encode(item)))
+      .catchError((error) {
+        print("Got error $error");
+      });
+  }
+
+  Future delete(Item item) {
+    print("In backend.delete");
+
+    // Make request to delete item
+    return _http.delete("${_baseUrl}/todos/delete/${item.id}")
+      .catchError((error) {
+        print("Got error $error");
+      });
+  }
+}
+
